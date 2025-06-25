da0ef294841d69de53608ff80870c0f5171439ab
jP <silva.joao@icen.ufpa.br>
2025-06-24 23:32:31 -0300

    first commit

---
+++ src/Hero.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Hero extends SerVivo{
+    String classe;
+    public Hero(String classe){
+        switch (classe) {
+            case "Guerreiro";
+                break;
+            case "Mago":
+                break;
+            case "ladino":
+                break;
+        }
+
+    }
+    private void setStatus(){
+    Random aleatorio = new Random();
+
+
+    }
+
+}
---
+++ src/SerVivo.java
@@ -0,0 +1,9 @@
+public class SerVivo {
+    String nome;
+    float velocidade;
+    float forca;
+    int vida, defesa, destreza;
+    public void serVivo() {
+
+    }
+}
